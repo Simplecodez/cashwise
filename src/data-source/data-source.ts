8af0fc43287c3fb8ada9fbca8970e47b100e1abd
Emmanuel Elendu <gozieelendu@gmail.com>
2024-12-20 20:56:42 +0100

    chore: add typeorm configs and datasource

---
+++ src/data-source/data-source.ts
@@ -0,0 +1,19 @@
+import { DataSource } from "typeorm";
+import TypeOrmConfig from "../configs/typeorm/typeorm.config";
+import { singleton } from "tsyringe";
+
+@singleton()
+export class DatabaseConnection {
+  private datasource: DataSource;
+  constructor() {
+    this.datasource = new DataSource(TypeOrmConfig.getOrmConfig());
+  }
+
+  async initialize() {
+    return this.datasource.initialize();
+  }
+
+  getDataSource() {
+    return this.datasource;
+  }
+}
