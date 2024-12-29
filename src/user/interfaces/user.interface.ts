da098e0fe0715cc64371388b65aab7667966ab07
Emmanuel Elendu <gozieelendu@gmail.com>
2024-12-29 16:39:05 +0100

    chore: extend the Request Object

--- src/user/interfaces/user.interface.ts
+++ src/user/interfaces/user.interface.ts
@@ -1,5 +1,10 @@
+import { Request } from 'express';
 import { User } from '../entities/user.entity';
 
 export interface IUser extends User {
   password: string;
+}
+
+export interface IRequest extends Request {
+  user: User;
 }
