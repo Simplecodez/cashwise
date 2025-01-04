85085995e5e6483dadf0a9ffd3f40f8cc68bb882
Emmanuel Elendu <gozieelendu@gmail.com>
2025-01-04 10:20:53 +0100

    chore: create bvn verification service

--- src/configs/di/container.ts
+++ src/configs/di/container.ts
@@ -1,10 +1,16 @@
-import { container } from 'tsyringe';
+import { container, Lifecycle } from 'tsyringe';
 import { Repository } from 'typeorm';
 import { User } from '../../user/entities/user.entity';
 import { DatabaseConnection } from '../../data-source/data-source';
 import { LocalAuth } from '../../user/entities/local-auth.entity';
+import { BvnService } from '../../integrations/bvn-verification/bvn-verification.service';
+import { Kyc } from '../../user/entities/kyc/kyc.entity';
 
 const datasource = container.resolve(DatabaseConnection);
+
+container.register('DataSource', {
+  useFactory: () => datasource.getDataSource()
+});
 
 container.register<Repository<User>>('UserRepository', {
   useFactory: () => datasource.getDataSource().getRepository(User)
@@ -14,6 +20,8 @@ container.register<Repository<LocalAuth>>('LocalAuthRepository', {
   useFactory: () => datasource.getDataSource().getRepository(LocalAuth)
 });
 
-container.register('DataSource', {
-  useFactory: () => datasource.getDataSource()
+container.register<Repository<Kyc>>('KycRepository', {
+  useFactory: () => datasource.getDataSource().getRepository(Kyc)
 });
+
+container.register('BvnService', BvnService, { lifecycle: Lifecycle.Singleton });
---
+++ src/integrations/bvn-verification/bvn-verification.service.ts
@@ -0,0 +1,46 @@
+import { singleton } from 'tsyringe';
+import { readFileSync } from 'fs';
+import { UserService } from '../../user/services/user/base-user.service';
+import { FindOneOptions } from 'typeorm';
+import { User } from '../../user/entities/user.entity';
+import {
+  BvnRecord,
+  IBvnService
+} from '../interfaces/bvn-verification.services.interface';
+import { CommonUtils } from '../../utils/common.utils';
+
+@singleton()
+export class BvnService implements IBvnService {
+  private static readonly mockBVNs: BvnRecord[] = JSON.parse(
+    readFileSync(`${__dirname}/../../mock-data/bvn-data.json`, { encoding: 'utf-8' })
+  );
+
+  constructor(private readonly userService: UserService) {}
+
+  private isMatched(user: User, bvnRecord: BvnRecord) {
+    return (
+      user.firstName.toLowerCase() === bvnRecord.firstName.toLowerCase() &&
+      user.lastName.toLowerCase() === bvnRecord.lastName.toLowerCase() &&
+      user.phoneNumber.replace(/^\+234/, '0') === bvnRecord.phoneNumber &&
+      CommonUtils.formatDate(user.dateOfBirth) === bvnRecord.phoneNumber
+    );
+  }
+
+  async verifyBvn(bvn: string, userId: string) {
+    const bvnRecord = BvnService.mockBVNs.find((value) => value.bvn === bvn);
+    if (!bvnRecord) {
+      return { isVerified: false, rejectionReason: 'Invalid BVN' };
+    }
+    const options: FindOneOptions<User> = {
+      where: { id: userId }
+    };
+
+    const user = await this.userService.findUserByOptions(options);
+    if (!user) return { isVerified: false, rejectionReason: 'Invalid BVN' };
+
+    if (!this.isMatched(user, bvnRecord))
+      return { isVerified: false, rejectionReason: 'Details mismatch' };
+
+    return { isVerified: true };
+  }
+}
