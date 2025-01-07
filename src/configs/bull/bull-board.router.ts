d7f9bb513c9e9cd7b89203b4c4491364de20de51
Emmanuel Elendu <gozieelendu@gmail.com>
2025-01-07 12:42:13 +0100

    chore: add kycQueue to the bullboard

--- src/configs/bull/bull-board.router.ts
+++ src/configs/bull/bull-board.router.ts
@@ -4,6 +4,7 @@ import { createBullBoard } from '@bull-board/api';
 import { Queue } from 'bullmq';
 import { singleton } from 'tsyringe';
 import { CommunicationQueue } from '../../communication/job-processor/communication.queue';
+import { KycQueue } from '../../user/job-processors/kyc.queue';
 
 @singleton()
 export class BullBoardRouter {
@@ -11,9 +12,12 @@ export class BullBoardRouter {
   private queueList: BullAdapter[];
   private serverAdapter: ExpressAdapter;
 
-  constructor(private readonly communicationQueue: CommunicationQueue) {
+  constructor(
+    private readonly communicationQueue: CommunicationQueue,
+    private readonly kycQueue: KycQueue
+  ) {
     this.serverAdapter = new ExpressAdapter();
-    this.queues = [this.communicationQueue.getQueue()];
+    this.queues = [this.communicationQueue.getQueue(), kycQueue.getQueue()];
     this.queueList = this.queues.map((queue) => new BullAdapter(queue));
     createBullBoard({
       queues: this.queueList,
