e2d6e64fbab98cbd6aecab6f6f5231b8f4dbdc1b
Emmanuel Elendu <gozieelendu@gmail.com>
2025-01-10 16:56:09 +0100

    chore: add Account controller

--- src/account/services/account.service.ts
+++ src/account/services/account.service.ts
@@ -1,6 +1,7 @@
 import { inject, singleton } from 'tsyringe';
-import { Repository } from 'typeorm';
+import { FindOneOptions, Repository } from 'typeorm';
 import { Account } from '../entities/account.entity';
+import { createHash, randomBytes } from 'crypto';
 
 @singleton()
 export class AccountService {
@@ -9,7 +10,51 @@ export class AccountService {
   ) {}
 
   async createAccount(data: Partial<Account>) {
-    const newAccount = this.accountRepository.create(data);
+    const accountNumber = await this.generateAccountNumber();
+    const newAccount = this.accountRepository.create({ ...data, accountNumber });
     return this.accountRepository.insert(newAccount);
+  }
+
+  async findOne(options: FindOneOptions<Account>) {
+    return this.accountRepository.findOne(options);
+  }
+
+  async confirmAccount(accountNumber: string) {
+    console.log(accountNumber);
+    return this.accountRepository
+      .createQueryBuilder('account')
+      .leftJoinAndSelect('account.user', 'user')
+      .select(['account.name', 'user.firstName', 'user.lastName'])
+      .where('account.accountNumber = :accountNumber', { accountNumber })
+      .getOne();
+  }
+
+  async findAll(userId: string) {
+    return this.accountRepository.find({ where: { userId } });
+  }
+
+  private async generateAccountNumber(): Promise<string> {
+    let exists = false;
+    let checkCount = 0;
+    let accountNumber = '';
+
+    do {
+      const timestamp = Date.now().toString();
+      const random = randomBytes(8).toString('hex');
+      const combined = `${timestamp}${random}`;
+
+      const hash = createHash('sha256').update(combined).digest('hex');
+
+      accountNumber = hash.replace(/\D/g, '').slice(0, 10);
+      const options: FindOneOptions<Account> = {
+        where: {
+          accountNumber
+        }
+      };
+      exists = (await this.findOne(options)) !== null;
+      checkCount++;
+    } while (exists && checkCount < 3);
+
+    return accountNumber;
   }
 }
