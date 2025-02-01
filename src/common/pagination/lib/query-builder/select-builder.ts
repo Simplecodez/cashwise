5744f91b793815d68402e908b6dd8095b9a8ef86
Emmanuel Elendu <gozieelendu@gmail.com>
2025-02-01 11:40:25 +0100

    feat: improve pagination validation and filter

--- src/common/pagination/lib/query-builder/select-builder.ts
+++ src/common/pagination/lib/query-builder/select-builder.ts
@@ -4,10 +4,11 @@ import { FiltersExpression } from '../interface/filters-expression.input';
 export class SelectBuilder<T extends object> {
   constructor(
     private readonly qb: SelectQueryBuilder<T>,
-    private filterExpression: FiltersExpression
+    private filterExpression?: FiltersExpression
   ) {}
 
   build() {
+    if (!this.filterExpression) return;
     const selectFieldArray = this.filterExpression.filters?.map((filter) => {
       if (!filter.selectFields || filter.selectFields.length === 0) {
         return [];
