3eae7734ea180744fe9363085ec2f99659c58f09
Simplecodez <gozieelendu@gmail.com>
2025-02-16 15:21:20 +0100

    chore: add logging to communication processor

--- src/communication/job-processor/communication.processor.ts
+++ src/communication/job-processor/communication.processor.ts
@@ -3,17 +3,25 @@ import { CommunicationService } from '../communications.service';
 import { CommunicationMedium } from '../communications.enum';
 import { singleton } from 'tsyringe';
 import { BullmqJobError } from '../../utils/job-error.utils';
+import { Logger } from '../../common/logger/logger';
 
 @singleton()
 export class CommunicationProcessor {
-  constructor(private readonly communicationService: CommunicationService) {}
+  constructor(
+    private readonly communicationService: CommunicationService,
+    private readonly logger: Logger
+  ) {}
 
   async process(job: Job): Promise<void> {
     switch (job.name) {
       case CommunicationMedium.EMAIL: {
         try {
           await this.communicationService.sendEmail(job.data);
+          this.logger.appLogger.info('Email sent');
         } catch (error: any) {
+          this.logger.appLogger.error(
+            `Emailing failed: ${error.message}: Timestamp: ${new Date().toISOString()}`
+          );
           throw new BullmqJobError(error.message, 'Nodemailer');
         }
         break;
@@ -22,7 +30,11 @@ export class CommunicationProcessor {
       case CommunicationMedium.SMS: {
         try {
           await this.communicationService.sendSMS(job.data.phoneNumber);
+          this.logger.appLogger.info('SMS sent');
         } catch (error: any) {
+          this.logger.appLogger.error(
+            `SMS failed: ${error.message}: Timestamp: ${new Date().toISOString()}`
+          );
           throw new BullmqJobError(error.message, 'Twilio');
         }
         break;
